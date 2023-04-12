54b978b68d0edd95c53041fd46b96669394adb87
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-04-12 10:42:49 +0530

    hello pattern data type

---
+++ getting_started.rb
@@ -0,0 +1,21 @@
+# hello Ruby 
+puts "hello fucking ruby";
+print "hello madafaka ruby";
+print " hello rony ruby";
+p  "hello rony ruby";
+
+# Pattern
+
+puts "    *    ";
+puts "   ***   ";
+puts "  *****  ";
+puts " ******* ";
+puts "*********";
+
+# variables
+
+name_variable = "ram"
+name_variable1 = "sohan"
+puts (name_variable +" "+name_variable1)
+
+# DATA TYPE
