6358caecd4f069fdf7264888096dd058011b86ca
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-04-12 11:32:26 +0530

    math methods

--- getting_started.rb
+++ getting_started.rb
@@ -67,3 +67,34 @@ puts name_variable1[0,15];
 # finding index
 puts name.index("A"); # expected nothing since A doesnot exist in name
 puts name.index("m"); # 0
+
+############### numbers methods and math ###############
+puts 5;
+puts 1.5;
+puts -1.5;
+puts  5 + 5;
+puts  5 * 5;
+print "unexpected operation "
+puts  5^6;
+print "power operation "
+puts  5**6;
+print "modulo operation "
+puts  6%5;
+print "division operation "
+puts  -6 % 5
+int = 45
+float = 45.56
+negetive_int = -45
+puts negetive_int.abs();
+print "round off";
+puts float.round();
+print "smallest integer funtion ";
+puts float.floor();
+print "greatest integer funtion ";
+puts float.ceil();
+print "squre root"
+puts Math.sqrt(int)
+print "float + integer "
+puts int + float
+print "significent number "
+puts (int + float).round(0)
