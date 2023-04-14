911140862bb44b2a0528a49d7477687e6095334f
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-04-14 11:13:25 +0530

    calculator with + - / % *

--- getting_started2.rb
+++ getting_started2.rb
@@ -1,1 +1,21 @@
 ####################################### Better Calculator in gettting_started.rb 2 ##########################
+puts "Enetr your first number : "
+num1 = gets.chomp().to_f
+puts "Enetr your second number : "
+num2 = gets.chomp().to_f
+puts "Enetr your operator number : "
+operator = gets.chomp()
+
+if operator==="+"
+    puts num1 + num2
+elsif  operator === "-"
+    puts num1 - num2
+elsif  operator === "*"
+    puts num1 * num2
+elsif  operator === "/"
+    puts num1 / num2
+elsif  operator === "%"
+    puts num1 % num2
+else 
+    puts "invalid oprator"
+end
