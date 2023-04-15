1d4f582d94411d4509fec2270fdbb7e379d03044
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-04-15 20:22:14 +0530

    merge conflicts

---
+++ loops.rb
@@ -0,0 +1,13 @@
+####################################### loops ############################################
+index = 1
+while index < 50
+    puts index
+    index +=1
+end
+
+
+friends = ["naman", "bob", "rony", "andrea", "larrisa"]
+
+for friend in friends
+    puts friend
+end
