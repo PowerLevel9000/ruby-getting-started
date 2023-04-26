96c58e99808c58ac5c9ea7e5f541b8d4fab48c37
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-04-26 17:29:31 +0530

    reading and writing the file

--- data_saver.rb
+++ data_saver.rb
@@ -1,12 +1,14 @@
+require 'json'
 def add_car_to_json
   cars_array = File.exist?('cars.json') ? JSON.parse(File.read('cars.json')) : []
   cars_array << { modal: @modal, name: @name, owner: @owner }
   File.write('cars.json', JSON.pretty_generate(cars_array))
 end
 
-def read_file
-  File.open('cars.json', 'r') do |f|
+def json_reader(file)
+  File.open("#{file}.json", 'r') do |f|
     data = JSON.parse(f.read)
-    puts data[0]
   end
 end
+
+puts json_reader("copy")
